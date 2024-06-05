d26ae3bfbe5d0159bbe0640c7217de18c884a499
Matias <matiasodoy072@gmail.com>
2024-06-04 21:30:57 -0400

    init

---
+++ src/App/store.ts
@@ -0,0 +1,10 @@
+import {configureStore} from '@reduxjs/toolkit';
+// Importa tus reducers aquí
+
+const store = configureStore({
+    reducer: {
+        // Define tus reducers aquí
+    },
+});
+
+export default store;
